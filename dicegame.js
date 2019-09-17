45c97c21a7b23d19d4c7be3dec6b250244d63956
Chao Yang <wamengyang1@yahoo.com>
2019-09-17 09:03:39 -0500

    re edit roll die function, now giving random numbers

--- dicegame.js
+++ dicegame.js
@@ -1,8 +1,10 @@
 "use strict"
 
 function rollDie(numberSides){ //function name(parameter)
-	let sides = Math.floor(Math.random() + numberSides + 1)  //var sides = Round #/returns random # between number of sides(+1 includes last #)
+	let sides = Math.floor(Math.random() * numberSides + 1)  //var sides = Round #/returns random # between number of sides(+1 includes last #)
 		return sides;
 }
 
 console.log(rollDie(20));
+
+
