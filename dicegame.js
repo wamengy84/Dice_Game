b8d15b89bfe5cabf77f4b3da8a997ef6a6ba7ee9
Chao Yang <wamengyang1@yahoo.com>
2019-09-17 11:58:19 -0500

    first die function update

--- dicegame.js
+++ dicegame.js
@@ -1,10 +1,45 @@
 "use strict"
 
-function rollDie(numberSides){ //function name(parameter)
-	let sides = Math.floor(Math.random() * numberSides + 1)  //var sides = Round #/returns random # between number of sides(+1 includes last #)
-		return sides;
+// function rollDie(numberSides){ //function name(parameter)
+// 	let side = Math.floor(Math.random() * numberSides + 1)  //var sides = Round #/returns random # between number of sides(+1 includes last #)
+// 		return sides;
+// }
+
+// console.log(rollDie(20));
+
+
+
+function rollDieWoods(numberSides){
+	let side = Math.floor(Math.random() * numberSides + 1)
+		let driverDistance = [400, 300, 200];
+		let distanceTotalYards = 1150;
+		let distanceUpdate = distanceTotalYards - ;
+		return side;
 }
 
-console.log(rollDie(20));
+console.log(rollDieWoods(3));
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function runGame() {
+//     // runs the game
+// }
